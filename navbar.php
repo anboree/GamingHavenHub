8d0e7c76b52fea244ac2658240a680dadbed55ec
anboree <zakisdavis423@gmail.com>
2026-02-23 15:45:03 +0200

    First commit

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <nav id="navbar">
+        <!-- Logo & Title -->
+        <a href="index.php"><img id="navbar-logo" src="assets/gaming-haven-hub-logo.png" alt="ghh logo" width="55px"></a>
+        <span id="navbar-header">GamingHavenHub</span>
+    </nav>
+</body>
+</html>
